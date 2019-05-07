5e7736d5451e4c14794518969a55e356b0dd46ef
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-07 02:28:55 -0400

    Created endpoint POST for logging in a user. Imported and added protected middleware to GET request for users

--- users/users-router.js
+++ users/users-router.js
@@ -2,8 +2,9 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 
 const Users = require('./users-model');
+const protected = require('../auth/protected');
 
-router.get('/users', (req, res) => {
+router.get('/users', protected, (req, res) => {
     Users
         .findUser()
         .then(users => {
@@ -26,6 +27,22 @@ router.post('/register', (req, res) => {
         .addUser(user)
         .then(userAdded => {
             res.status(201).json(userAdded);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
+router.post('/login', (req, res) => {
+    let { username, password } = req.body;
+    Users
+        .findUserBy({ username })
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                res.status(200).json({ user, message: `${user.username} has logged in!` });
+            } else {
+                res.status(404).json({ message: "Username and/or password are not valid." });
+            }
         })
         .catch(err => {
             res.status(500).json(err);
